e716ce391c3a7c464cf51f767e6115f5131c6dee
Patrick Steadman <ptsteadman@gmail.com>
2016-07-15 04:30:47 -0400

    use hashHistory to make site work as static site

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
 import App from './App.jsx';
 import Home from './Home.jsx';
 import SlideIntro from './SlideIntro.jsx';
 
 ReactDOM.render((
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="slide-1" component={SlideIntro} />
